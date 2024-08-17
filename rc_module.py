67231dbe50d6999d0e45162388867260b8144e3a
JohnyWalker167 <150372877+JohnyWalker167@users.noreply.github.com>
2024-08-18 04:57:07 +0530

    added softmux

--- rc_module.py
+++ rc_module.py
@@ -355,6 +355,85 @@ async def changeindex(status, local_path, input_file_name, output_file_name, cus
     finally:
         process = None  # Clear the process reference in the end
 
+async def softmux(status, local_path, input_file_name, output_file_name, custom_title, audio_select, subtitle_file_name):
+   
+    input_file_path = os.path.join(local_path, input_file_name)
+    subtitle_file_path = os.path.join(local_path, subtitle_file_name)
+    output_file_path = os.path.join(local_path, output_file_name)
+    file_title = await remove_unwanted(output_file_name)
+
+    ffmpeg_command = [
+    'ffmpeg',
+    '-i', input_file_path,
+    '-i', subtitle_file_path,  # Include the SRT subtitle file
+    '-metadata', f'title={file_title}',
+    '-metadata:s:v:0', f'title={custom_title}',
+    '-metadata:s:a:0', f'title={custom_title}',
+    '-metadata:s:s:0', f'title={custom_title}',
+    '-c:v', 'copy',  # Copy video stream
+    '-c:a', 'copy',  # Copy audio stream
+    '-c:s', 'mov_text',  # Use mov_text codec for the subtitle
+    '-map', '0:v',  # Map the video stream
+    '-map', f'{audio_select}',  # Map the selected audio stream
+    '-map', '0:s',  # Map the original subtitle stream (if any)
+    '-map', '1',  # Map the new subtitle file
+    output_file_path
+    ]
+
+    last_text = None
+
+    try:
+        # Run the ffmpeg command and capture the output
+        process = subprocess.Popen(ffmpeg_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        
+        # Log output in real time
+        for line in process.stdout:
+            if process is None: # Check if the process has been cancelled
+              break
+            line = line.strip()
+
+            # Parse the ffmpeg progress output
+            frame_match = re.search(r'frame=\s*(\d+)', line)
+            fps_match = re.search(r'fps=\s*(\d+\.?\d*)', line)
+            size_match = re.search(r'size=\s*([\d\.]+(?:kB|MB|GB))', line)
+            time_match = re.search(r'time=(\d{2}:\d{2}:\d{2}\.\d{2})', line)
+            bitrate_match = re.search(r'bitrate=\s*([\d\.]+kbits/s)', line)
+            speed_match = re.search(r'speed=\s*([\d\.]+x)', line)
+
+            if frame_match and fps_match and size_match and time_match and bitrate_match and speed_match:
+                frame = int(frame_match.group(1))
+                fps = float(fps_match.group(1))
+                size = convert_size_to_mb(size_match.group(1))
+                time_str = time_match.group(1)
+                bitrate = bitrate_match.group(1)
+                speed_str = speed_match.group(1)
+
+                text = (f'**Frame**: {frame} | **FPS**: {fps} | **Size**: {size:.2f} MB | '
+                        f'**Time**: {time_str} | **Bitrate**: {bitrate} | **Speed**: {speed_str}')
+
+                if text != last_text:
+                    await status.edit_text(text)
+                    last_text = text
+
+                await asyncio.sleep(3)
+
+
+        if process is not None:  # Ensure process is still running before waiting
+            process.wait()
+
+            if process.returncode == 0:
+              await status.edit_text(f"Change Index successfully `{output_file_path}`")
+            else:
+              await status.edit_text(f"ffmpeg command failed with return code {process.returncode}")
+        else:
+           await status.delete
+        
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Error: {e}")
+        return None
+    finally:
+        process = None  # Clear the process reference in the end
+
 async def upload(status, local_file, remote_path, remote_name='remote', rclone_config_path=None):
     """
     Upload a local file to a specified path on a cloud storage using rclone.
